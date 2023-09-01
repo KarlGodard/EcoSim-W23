7603a72afefd1b3da004c30cea1c7663febf21b2
Karl Godard <godardk@umich.edu>
2023-09-01 13:57:02 -0400

    updated parameters

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from AnimalParams import SimulationParams, MapParams, AnimalParams
 
 simulationParams = SimulationParams()
 simulationParams.simulationLength = 1000
-simulationParams.simulationSpeed = 0  #delay in ms
+simulationParams.simulationSpeed = 20  #delay in ms
 
 mapParams = MapParams()
-mapParams.sizeX = 50
-mapParams.sizeY = 30
+mapParams.sizeX = 80
+mapParams.sizeY = 50
 mapParams.percentWaterCoverage = 30
-mapParams.numStartingPredators = 0
-mapParams.numStartingPrey = 120
+mapParams.numStartingPredators = 80
+mapParams.numStartingPrey = 200
 mapParams.temp = 70
 
 predatorParams = AnimalParams()
@@ -32,7 +32,7 @@ preyParams = AnimalParams()
 preyParams.maxFood = 100
 preyParams.maxWater = 100
 preyParams.minReproductiveAge = 10
-preyParams.reproductiveDelay = 4
+preyParams.reproductiveDelay = 10
 preyParams.waterSearchRadius = 15
 preyParams.foodSearchRadius = 15
 preyParams.reproductiveSearchRadius = 15
